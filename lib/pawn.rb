21328e7c835e2f1ba9d175eec5ff74ba59d8da75
Krzysztof Piotrowski <krzysiekp91@gmail.com>
2016-05-22 11:51:22 +0200

    Adds 'first_move' variable for Pawn class

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -1,7 +1,9 @@
 class Pawn < Piece
   attr_reader :symbol
+  attr_accessor :first_move
   def initialize (color, position)
     super(color, position)
     @symbol = Piece.set_symbol(color, "P", "p")
+    @first_move = true
   end
 end
